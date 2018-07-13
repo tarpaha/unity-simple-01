d6174dfddb64136e586175e175fe9108789e68e4
tarpaha <ppopchikovsky@gmail.com>
2018-07-13 16:29:48 +0300

    initial commit

---
+++ Assets/Scripts/SimpleController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class SimpleController : MonoBehaviour
+{
+    private void FixedUpdate()
+    {
+        if (Input.anyKey)
+        {
+            gameObject.GetComponent<Rigidbody>().AddForce(-2.0f * Physics.gravity);
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        gameObject.GetComponent<Renderer>().material.color = Color.red;
+    }
+
+    private void OnCollisionStay(Collision other)
+    {
+        gameObject.GetComponent<Renderer>().material.color = Color.red;
+    }
+    
+    private void OnCollisionExit(Collision other)
+    {
+        gameObject.GetComponent<Renderer>().material.color = Color.white;
+    }
+}
